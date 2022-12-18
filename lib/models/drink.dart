11993a4dcc86c4440840a54ef9014a1e90081c6f
Michael Nascimento <michael.nsc@outlook.com>
2022-12-18 11:17:01 -0300

    Rename models

--- lib/models/drink.dart
+++ lib/models/drink.dart
@@ -2,10 +2,16 @@ class Drink {
  late String name;
  late String thumb;
  late String id;
+ late String category;
+ late String alcoholic;
+ late String instructions;
 
   Drink.fromJson(Map<String, dynamic> json) {
     name = json['strDrink'];
     thumb = json['strDrinkThumb'];
     id = json['idDrink'];
+    category = json['strCategory'];
+    alcoholic = json['strAlcoholic'];
+    instructions = json['strInstructions'];
   }
 }
