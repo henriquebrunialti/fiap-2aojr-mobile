3de9c536e0f189079c8dbe7d51073de0de6a1427
Michael Nascimento <michael.nsc@outlook.com>
2022-12-19 17:47:58 -0300

    create constants file

---
+++ lib/constants/text.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+const headingStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+const subheadingStyle = TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
